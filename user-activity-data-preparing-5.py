ed80a282b8a399f000a0125c53af22208d3ea7d4
enti <szabo.bence.tat@gmail.com>
2018-10-15 10:43:52 +0200

    user-submission-cnt.csv generating script added

---
+++ user-activity-data-preparing-5.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+user_activity = pd.read_csv('user-activity.csv', delim_whitespace=True,
+                            parse_dates=True, infer_datetime_format=True, # usecols=range(0, 250),  # nrows=1000,
+                            ).set_index(['userid']).drop(['churn'], axis=1)
+
+print(user_activity.columns)
+print(user_activity.head())
+
+submissions = user_activity.count(axis=1)
+
+user_submission_count = user_activity.iloc[:, 0:2]
+user_submission_count.loc[:, 'submissions'] = user_activity.count(axis=1)-1
+user_submission_count = user_submission_count.reset_index()
+
+user_submission_count.iloc[:, :].to_csv('user-submission-cnt.csv', index=False, sep='\t')
+
